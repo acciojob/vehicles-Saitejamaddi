2a141db8e70d8fcdcf817950b32db54c8777a740
Saitejamaddi <saitejamaddi123@gmail.com>
2025-12-19 09:43:29 +0530

    added first gear in constructor

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -46,6 +46,7 @@ public class Car extends Vehicle {
         this.gears = gears;
         this.isManual = isManual;
         this.seats = seats;
+        this.currentGear = 1;
     }
 
     public void changeGear(int newGear) {
